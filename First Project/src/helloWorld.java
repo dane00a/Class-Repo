598543aaf804d53fdd91537496fb9fe052782d62
Dane <dane.andrews99@gmail.com>
2015-07-08 15:51:57 -0500

    Java Begins
    We've got a lot of stuff coming up in the object-oriented
    code-once-run-everywhere department: Java. Here's a commit of Hello
    World and a bunch of associated project and the like.

---
+++ First Project/src/helloWorld.java
@@ -0,0 +1,22 @@
+
+public class helloWorld {
+	public static void main(String []args){
+		System.out.println("Hello World");
+		int n1=0, n2=1, n3, i, count=47;
+		System.out.print(n1+" "+n2);
+		for (i=2; i<count; ++i){
+			n3 = n1 + n2;
+			System.out.print(" "+ n3);
+			n1 = n2;
+			n2 = n3;
+		}
+		int r1=0,r2=1,r3,count2=666;
+		System.out.print(r1 + " " + r2);
+		for (int k=0; k<count;k++){
+			n3 = n1 + n2;
+			if (n3 >= 10){
+				
+			}
+		}
+	}
+}
