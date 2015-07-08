28ab2a14b0753130bd60e3ef5af5411705745d06
Dane <dane.andrews99@gmail.com>
2015-07-08 11:48:02 -0500

    Qualified!
    Did the Qualifications Page
    Put an !important override in the menu to prevent the menu from breaking
    Site is pretty much done for this v1. Will do an aesthetic touch-up, and
    that'll be it.

---
+++ Resume/qualifications.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link href="css/common.css" rel="stylesheet" type="text/css">
+    <link href="css/home.css" rel="stylesheet" type="text/css">
+    <link href="css/qual.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+      <?php include("menu.html"); ?>
+      <div class="cont">
+        <h1>Qualifications</h1>
+          <p>
+            I got 'em.
+          </p>
+      </div>
+      <hr>
+      <div class="cont">
+        <h3>Here's a list of my qualifications in Programming:</h3>
+        <p>
+          Languages I Can Use:
+        </p>
+        <ul>
+          <li>
+            HTML
+          </li>
+          <li>
+            CSS
+          </li>
+          <li>
+            PHP
+          </li>
+          <li>
+            SQL
+          </li>
+          <li>
+            Opsimai*
+          </li>
+        </ul>
+        <p>
+          Languages I will soon be investing in:
+        </p>
+        <ul>
+          <li>
+            Javascript and Associated Libraries (JQuery, etc.)
+          </li>
+          <li>
+            XML
+          </li>
+          <li>
+            AJAX
+          </li>
+        </ul>
+        <p>
+          *A note on Opsimai: <br>
+          You probably haven't heard of it. That's fine. Currently, it's an IoT language I and a friend of mine are creating, HOWEVER we need to get our
+          Flex-Lexical Parser up and running before it becomes of any significant use. Because C makes me cry and time constraints, the project is on hold, but eh,
+          I think it's still worth noting. Stay tuned.
+        </p>
+      </div>
+  </body>
+</html>
