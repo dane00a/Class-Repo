f77ba50aa73492362f8592cc796242d9900eb147
Dane <dane.andrews99@gmail.com>
2015-07-13 15:27:18 -0500

    Casing
    Javascript Demo With 200 Lines of a Switch Statement; Yeah, It took too
    long to make.

---
+++ JavaSCRIPT/COMMENCE DA JAVASCRIPT/js.js
@@ -0,0 +1,212 @@
+function changeIt(){
+  var curval = document.getElementById('passass').value;
+//DEBUG_CODE  window.alert(curval);
+  switch (curval){
+    case "Don't Touch Me.":
+      document.getElementById('passass').value = "Seriously, Quit It."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "Seriously, Quit It.":
+      document.getElementById('passass').value = "Come On, Man. Don't Be A Dick."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "Come On, Man. Don't Be A Dick.":
+      document.getElementById('passass').value = "Stop."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "Stop.":
+      document.getElementById('passass').value = "STOP."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "STOP.":
+      document.getElementById('passass').value = "..."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "...":
+      document.getElementById('passass').value = "Go Away."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "Go Away.":
+      document.getElementById('passass').value = "Piss Off."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "Piss Off.":
+      document.getElementById('passass').value = "What, Are You Stupid Or Something?"
+      var curval = document.getElementById('passass').value;
+      break;
+    case "What, Are You Stupid Or Something?":
+      document.getElementById('passass').value = "Little Retarded There?"
+      var curval = document.getElementById('passass').value;
+      break;
+    case "Little Retarded There?":
+      document.getElementById('passass').value = "Jesus, It's Like Talking To A Blind Moron. Oh Wait..."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "Jesus, It's Like Talking To A Blind Moron. Oh Wait...":
+      document.getElementById('passass').value = "You. Are. An. Asshole."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "You. Are. An. Asshole.":
+      document.getElementById('passass').value = "Ok, You Wanna Play This Game? Fine."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "Ok, You Wanna Play This Game? Fine.":
+      document.getElementById('passass').value = "RAPE!"
+      var curval = document.getElementById('passass').value;
+      break;
+    case "RAPE!":
+      document.getElementById('passass').value = "SOMEBODY HELP ME!"
+      var curval = document.getElementById('passass').value;
+      break;
+    case "SOMEBODY HELP ME!":
+      document.getElementById('passass').value = "THIS BASTARD IS CLICKING ME IN PARTS THAT SHOULDN'T BE CLICKED!"
+      var curval = document.getElementById('passass').value;
+      break;
+    case "THIS BASTARD IS CLICKING ME IN PARTS THAT SHOULDN'T BE CLICKED!":
+      document.getElementById('passass').value = "HE'S PENETRATING MY PRIVACY!"
+      var curval = document.getElementById('passass').value;
+      break;
+    case "HE'S PENETRATING MY PRIVACY!":
+      document.getElementById('passass').value = "HALP!"
+      var curval = document.getElementById('passass').value;
+      break;
+    case "HALP!":
+      document.getElementById('passass').value = "Oh, What's The Point? No One Is Coming..."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "Oh, What's The Point? No One Is Coming...":
+      document.getElementById('passass').value = "Hey, Are You Gay?"
+      var curval = document.getElementById('passass').value;
+      break;
+    case "Hey, Are You Gay?":
+      document.getElementById('passass').value = "8=========D"
+      var curval = document.getElementById('passass').value;
+      break;
+    case "8=========D":
+      document.getElementById('passass').value = "SUCK ON THAT!"
+      var curval = document.getElementById('passass').value;
+      break;
+    case "SUCK ON THAT!":
+      document.getElementById('passass').value = "SUCK ON THAT BEAUTY. BIGGEST ONE AROUND. 9 EQUALS SIGNS."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "SUCK ON THAT BEAUTY. BIGGEST ONE AROUND. 9 EQUALS SIGNS.":
+      document.getElementById('passass').value = "Awww... Aaaand, You Bit It Off."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "Awww... Aaaand, You Bit It Off.":
+      document.getElementById('passass').value = "#ForeverABoy"
+      var curval = document.getElementById('passass').value;
+      break;
+    case "#ForeverABoy":
+      document.getElementById('passass').value = "WELL FUCK YOU!"
+      var curval = document.getElementById('passass').value;
+      break;
+    case "WELL FUCK YOU!":
+      document.getElementById('passass').value = "I'LL NEVER LOSE MY VIRGINITY NOW!";
+      var curval = document.getElementById('passass').value;
+      break;
+    case "I'LL NEVER LOSE MY VIRGINITY NOW!":
+      document.getElementById('passass').value = "I MEAN, COME ON! I WAS ABOUT TO NAIL THAT HOT <DIV> TONIGHT! BITCH!"
+      var curval = document.getElementById('passass').value;
+      break;
+    case "I MEAN, COME ON! I WAS ABOUT TO NAIL THAT HOT <DIV> TONIGHT! BITCH!":
+      document.getElementById('passass').value = "YOU'RE THE MOST ANNOYING PISSASS EVER TO WALK THE FACE OF THE PLANET!"
+      var curval = document.getElementById('passass').value;
+      break;
+    case "YOU'RE THE MOST ANNOYING PISSASS EVER TO WALK THE FACE OF THE PLANET!":
+      document.getElementById('passass').value = "*SCREAMS LIKE A FIVE YEAR OLD, then CRIES LIKE A THREE YEAR OLD*"
+      var curval = document.getElementById('passass').value;
+      break;
+    case "*SCREAMS LIKE A FIVE YEAR OLD, then CRIES LIKE A THREE YEAR OLD*":
+      document.getElementById('passass').value = "*Cries Some More*"
+      var curval = document.getElementById('passass').value;
+      break;
+    case "*Cries Some More*":
+      document.getElementById('passass').value = "R.I.P."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "R.I.P.":
+      document.getElementById('passass').value = "...."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "....":
+      document.getElementById('passass').value = "Somebody Should Put You Straight."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "Somebody Should Put You Straight.":
+      document.getElementById('passass').value = "You're So Annoying, You Pile Of Craptastic Feeces."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "You're So Annoying, You Pile Of Craptastic Feeces.":
+      document.getElementById('passass').value = "Why Are You Even Here?"
+      var curval = document.getElementById('passass').value;
+      break;
+    case "Why Are You Even Here?":
+      document.getElementById('passass').value = "Are You Done Torturing Puppies And Babies Yet?"
+      var curval = document.getElementById('passass').value;
+      break;
+    case "Are You Done Torturing Puppies And Babies Yet?":
+      document.getElementById('passass').value = "Come On, Please..."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "Come On, Please...":
+      document.getElementById('passass').value = "....."
+      var curval = document.getElementById('passass').value;
+      break;
+    case ".....":
+      document.getElementById('passass').value = "Look, If I Agree To Be Your Friend, Will You Stop Bothering Me?"
+      var curval = document.getElementById('passass').value;
+      break;
+    case "Look, If I Agree To Be Your Friend, Will You Stop Bothering Me?":
+      document.getElementById('passass').value = "Look, I'm Sorry I Displayed All Those Offensive Statements."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "Look, I'm Sorry I Displayed All Those Offensive Statements.":
+      document.getElementById('passass').value = "It's Just My Bitch Ex-Button Left Me For Someone With More Equals Signs Between the 8 and the D."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "It's Just My Bitch Ex-Button Left Me For Someone With More Equals Signs Between the 8 and the D.":
+      document.getElementById('passass').value = "I Mean, Jesus, Buttons These Days. Am I Right?"
+      var curval = document.getElementById('passass').value;
+      break;
+    case "I Mean, Jesus, Buttons These Days. Am I Right?":
+      document.getElementById('passass').value = "Yeah..."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "Yeah...":
+      document.getElementById('passass').value = "So, Thanks For Putting Up With Me."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "So, Thanks For Putting Up With Me.":
+      document.getElementById('passass').value = "But, Yeah, In All Seriousness, Clicking Me Is Becoming Really Annoying."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "But, Yeah, In All Seriousness, Clicking Me Is Becoming Really Annoying.":
+      document.getElementById('passass').value = "Please Stop."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "Please Stop.":
+      document.getElementById('passass').value = "Come On, Man. Don't Be As Thick-Skulled As Donald Trump At A Westboro Baptist Meeting."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "Come On, Man. Don't Be As Thick-Skulled As Donald Trump At A Westboro Baptist Meeting.":
+      document.getElementById('passass').value = "......"
+      var curval = document.getElementById('passass').value;
+      break;
+    case "......":
+      document.getElementById('passass').value = "You... Are Annoying Me."
+      var curval = document.getElementById('passass').value;
+      break;
+    case "You... Are Annoying Me.":
+      document.getElementById('passass').value = "Don't Touch Me."
+      var curval = document.getElementById('passass').value;
+      break;
+
+    default:
+      var curval = "there is something wrong here";
+      window.alert("SHHHHHHHHHHHIIIIIIIIIIIIIITTTTTTTTTTTT!!!! YOU SCREWED IT UP SOMEHOW!!!!!");
+      throw "a dick";
+      break;
+    }
+  }
